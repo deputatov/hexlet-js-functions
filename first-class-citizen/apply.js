caa1c41cdb57b971d24fe70b4ce5439284b4ae52
Deputatov Nikita <deputatovn@yandex.ru>
2019-05-27 17:19:44 +0300

    Deleted console.log func

--- first-class-citizen/apply.js
+++ first-class-citizen/apply.js
@@ -8,10 +8,3 @@ const apply_v1 = (n, f, a) => { //n - number or quantity, f - function, a - argu
 const apply_v2 = (n, f, a) => {
     return n === 0 ? a : apply_v2(n - 1, f, f(a))
 }
-
-console.log(apply_v2(0, Math.sqrt, 4));
-console.log(apply_v2(1, Math.sqrt, 4));
-console.log(apply_v2(2, Math.sqrt, 16));
-console.log(apply_v2(3, Math.sqrt, 256));
-console.log(apply_v2(1, v => v ** 2, 3)); // => 9
-console.log(apply_v2(5, v => v + 10, 3)); // => 53
